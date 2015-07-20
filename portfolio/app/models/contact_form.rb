327369838c2f63d09069c454578ac2f75b89c868
Mia Garbagnati <f.g.garbagnati@gmail.com>
2015-07-20 01:30:10 -0700

    fills out contact form model -- tested in console, but not actually delivering emails

--- portfolio/app/models/contact_form.rb
+++ portfolio/app/models/contact_form.rb
@@ -1,2 +1,14 @@
-class ContactForm < ActiveRecord::Base
+class ContactForm < MailForm::Base
+  attribute :name,        :validate => true
+  attribute :email,       :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :company
+  attribute :message,     :validate => true
+
+  def headers
+    {
+      :subject => "New Message",
+      :to => "f.g.garbagnati@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
 end
